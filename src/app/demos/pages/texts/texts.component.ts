43129997d72677bc1e75fb067537a7a4b80acdd3
Cristian <cristguevaraolaya@gmail.com>
2023-01-06 12:17:41 -0500

    texts animations

--- src/app/demos/pages/texts/texts.component.ts
+++ src/app/demos/pages/texts/texts.component.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TextsComponent implements OnInit {
 
+  div01:boolean=false
+  div02:boolean=false
+  div03:boolean=false
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+
 
 }
