0c12817eead507508a92d9b1c35792ade94f277e
Cristian <cristguevaraolaya@gmail.com>
2023-03-02 08:31:23 -0500

    delete login screen

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,15 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { ValidateTokenGuard } from './guards/validate-token.guard';
 
 const routes: Routes = [
-  {
-    path: 'auth',
-    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
-  },
+  // {
+  //   path: 'auth',
+  //   loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
+  // },
   {
     path: 'Componentes',
     loadChildren: () => import('./demos/demos.module').then(m => m.DemosModule),
-    canActivate: [ValidateTokenGuard],
-    canLoad: [ValidateTokenGuard]
+    // canActivate: [ValidateTokenGuard],
+    // canLoad: [ValidateTokenGuard]
   },
   {
     path: '**', redirectTo: 'Componentes'
